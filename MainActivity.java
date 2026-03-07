78498f9852a6be0e5f16bfab90bb672a0b894706
Azer <157844667+Azziikk1@users.noreply.github.com>
2026-03-07 23:13:12 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,26 @@
 package com.stackgame.app;
 
 import android.webkit.WebView;
+import android.webkit.JavascriptInterface;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+
+    public class AndroidBridge {
+        @JavascriptInterface
+        public void exitApp() {
+            finishAffinity();
+            System.exit(0);
+        }
+    }
+
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = getBridge().getWebView();
+        webView.addJavascriptInterface(new AndroidBridge(), "AndroidBridge");
+    }
+
     @Override
     public void onBackPressed() {
         WebView webView = getBridge().getWebView();
